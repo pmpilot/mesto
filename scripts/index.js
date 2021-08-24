ad05621e532ce865188323cc22e34c68cd5b7c9c
Pavel Maslennikov <pma.14@yandex.ru>
2021-08-24 22:39:31 +0400

    изменил класс popup_is-opened и popup__input_type_name

--- scripts/index.js
+++ scripts/index.js
@@ -7,18 +7,18 @@ let closePopupButton = document.querySelector('.popup__close');
 let formElement = document.querySelector(".popup__form");
 let introTitle = document.querySelector(".intro__title");
 let introSubtitle = document.querySelector(".intro__subtitle");
-let nameInput = document.querySelector(".popup__input_name");
-let jobInput = document.querySelector(".popup__input_job");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_job");
 
 /*popup*/
 /*открывает и закрываем окно редактирование*/
 function toggleForm() {
-    if (!popup.classList.contains('.popup__is-opened')){ /*! -условие с отрицанием*/
+    if (!popup.classList.contains('.popup_is-opened')){ /*! -условие с отрицанием*/
     nameInput.value = introTitle.textContent;
     jobInput.value = introSubtitle.textContent;
     }
 
-    popup.classList.toggle('popup__is-opened');
+    popup.classList.toggle('popup_is-opened');
 }
 
 /*input*/
